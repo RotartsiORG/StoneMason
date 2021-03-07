2a614a2e59e07c5995fb119fe84403d97667bfc8
Rotartsi <rotartsi0482@gmail.com>
2021-03-07 14:43:51 -0800

    update submodules

--- include/stms/rend/gl/gl_ImGui.hpp
+++ include/stms/rend/gl/gl_ImGui.hpp
@@ -12,8 +12,8 @@
 #   include "gl.hpp"
 
 #   include <imgui/imgui.h>
-#   include <imgui/examples/imgui_impl_glfw.h>
-#   include <imgui/examples/imgui_impl_opengl3.h>
+#   include <imgui/backends/imgui_impl_glfw.h>
+#   include <imgui/backends/imgui_impl_opengl3.h>
 
 #endif
 
--- src/stms/rend/gl/gl_ImGui.cpp
+++ src/stms/rend/gl/gl_ImGui.cpp
@@ -7,8 +7,9 @@
 
 #   include <imgui/imgui.cpp>
 #   include <imgui/imgui_demo.cpp>
+#   include <imgui/imgui_tables.cpp>
 #   include <imgui/imgui_draw.cpp>
 #   include <imgui/imgui_widgets.cpp>
-#   include <imgui/examples/imgui_impl_glfw.cpp>
-#   include <imgui/examples/imgui_impl_opengl3.cpp>
+#   include <imgui/backends/imgui_impl_glfw.cpp>
+#   include <imgui/backends/imgui_impl_opengl3.cpp>
 #endif
